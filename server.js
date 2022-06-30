940951efbaea6f8006bd5b60300edd7079b6b3ce
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-30 18:02:55 -0500

    aplico linter

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.port || 3000;
 
 //Requiere para usar Prisma
-const { PrismaClient } = require('@prisma/client');
-const { PrismaClientRustPanicError } = require('@prisma/client/runtime');
+const { PrismaClient } = require("@prisma/client");
+const { PrismaClientRustPanicError } = require("@prisma/client/runtime");
 const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-    res.json({ message: 'alive' });
+app.get("/", (req, res) => {
+    res.json({ message: "alive" });
 });
 
-app.get('/animals', async (req, res) => {
+app.get("/animals", async (req, res) => {
     const allAnimals = await prisma.animals.findMany({});
-    res.json(allAnimals)
+    res.json(allAnimals);
 });
 
-app.get('/animals/:id', async (req, res) => {
+app.get("/animals/:id", async (req, res) => {
     const id = req.params.id;
     const animal = await prisma.animals.findUnique({ where: { id: parseInt(id) } });
     res.json(animal);
 });
 
-app.post('/animals', async (req, res) => {
+app.post("/animals", async (req, res) => {
     const animal = {
         name: req.body.name,
         imagen: req.body.imagen,
         description: req.body.description
     };
-    const message = 'Animal creado.';
+    const message = "Animal creado.";
     await prisma.animals.create({ data: animal });
     return res.json({ message });
 });
 
-app.put('/animals/:id', async (req, res) => {
+app.put("/animals/:id", async (req, res) => {
     const id = parseInt(req.params.id);
 
     await prisma.animals.update({
@@ -46,11 +46,11 @@ app.put('/animals/:id', async (req, res) => {
             imagen: req.body.imagen,
             description: req.body.description
         }
-    })
+    });
     return res.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/animals/:id', async (req, res) => {
+app.delete("/animals/:id", async (req, res) => {
     const id = parseInt(req.params.id);
 
     await prisma.animals.delete({where: {id: id}});
